6265020e46be60af27346885ad3ede814221aa24
dmb-2222 <dmb-2222@yandex.ru>
2019-07-04 16:33:04 +0300

    v.1

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+let textarea = document.querySelector("textarea");
+textarea.addEventListener("keypress", textareaLengthCheck);
+const counter = document.querySelector(".col-2__counter--symbol");
+function textareaLengthCheck() {
+  let count = textarea.value.length + 1;
+  counter.innerHTML = count;
+}
+
+textarea.addEventListener("keydown", KeyCheck);
+function KeyCheck(event) {
+  const { keyCode } = event;
+  if (keyCode === 8 || 46) {
+    counter.innerHTML = textarea.value.length - 1;
+  }
+}
